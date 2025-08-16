8c089f5b65070c5a71c72fa78dc3050acc291f21
PRACHI JHA <179294335+CoderPrachijha@users.noreply.github.com>
2025-08-16 23:18:20 +0530

    Update badge.tsx

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
+
+
 interface BadgeProps extends React.HTMLAttributes<HTMLSpanElement> {
   variant?: "default" | "secondary" | "destructive" | "outline"
 }
